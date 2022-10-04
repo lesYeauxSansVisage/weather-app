97e31d94e94bb05be2c0631ac95fb5a32c40cd6e
Gen <genesishawkeye42@gmail.com>
2022-10-04 19:02:21 -0300

    added the function to get the data from open weather

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-const API_KEY = "b3d6abbbc5cc818b36dae477dd8ac490";
+// const API_KEY = "b3d6abbbc5cc818b36dae477dd8ac490";
 
-const getWeather = async (city) => {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-  );
+// const getWeather = async (city) => {
+//   const response = await fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+//   );
 
-  const weatherData = await response.json();
+//   const weatherData = await response.json();
 
-  console.log(weatherData);
-};
+//   const { weather } = await weatherData;
 
-getWeather("maranguape");
+//   console.log(weatherData);
+
+//   const weatherDiv = document.querySelector(".weather-data");
+
+//   console.log(weather);
+
+//   weatherDiv.innerHTML = `<div>${weather[0].description}</div>
+//   <img src="http://openweathermap.org/img/w/${weather[0].icon}.png" />`;
+// };
+
+// getWeather("maranguape");
+
+// const weatherForm = document.querySelector("#location-form");
+
+// weatherForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const location = document.querySelector("#location-input");
+
+//   getWeather(location.value);
+// });
