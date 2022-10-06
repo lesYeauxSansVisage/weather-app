c34b897f63bb5ad04c4877f1bb6b82a7f3e6cfca
Gen <genesishawkeye42@gmail.com>
2022-10-06 15:15:20 -0300

    added the loader component

--- src/index.js
+++ src/index.js
@@ -1,32 +1,60 @@
-// const API_KEY = "b3d6abbbc5cc818b36dae477dd8ac490";
+const API_KEY = "b3d6abbbc5cc818b36dae477dd8ac490";
 
-// const getWeather = async (city) => {
-//   const response = await fetch(
-//     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-//   );
+const getWeather = async (city) => {
+  loaderComponent();
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    );
 
-//   const weatherData = await response.json();
+    const weatherData = await response.json();
 
-//   const { weather } = await weatherData;
+    console.log(weatherData);
 
-//   console.log(weatherData);
+    const weatherDiv = document.querySelector(".weather-data");
 
-//   const weatherDiv = document.querySelector(".weather-data");
+    weatherDiv.innerHTML = createWeatherData(weatherData);
+  } catch (error) {
+    console.log(error);
 
-//   console.log(weather);
+    const weatherDiv = document.querySelector(".weather-data");
 
-//   weatherDiv.innerHTML = `<div>${weather[0].description}</div>
-//   <img src="http://openweathermap.org/img/w/${weather[0].icon}.png" />`;
-// };
+    weatherDiv.innerHTML =
+      "Sorry, we couldn't find the place youre searching for.";
+  }
+  loaderComponent();
+};
 
-// getWeather("maranguape");
+getWeather("maranguape");
 
-// const weatherForm = document.querySelector("#location-form");
+const weatherForm = document.querySelector("#location-form");
 
-// weatherForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-//   const location = document.querySelector("#location-input");
+  const location = document.querySelector("#location-input");
 
-//   getWeather(location.value);
-// });
+  getWeather(location.value);
+});
+
+function createWeatherData(data) {
+  return `<h2>${data.name}</h2>
+    <div class="weather-data__icon">
+        <div class="icon">
+            <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="rain-icon">
+        </div>
+        <p class="description">${data.weather[0].description}</p>
+        <p class="main-temp">${data.main.temp}<span>º</span></p>
+
+    </div>
+    <div class="weather-data__temperature">
+        <p class="min-temp"><span>MIN</span>${data.main.temp_min}<span>º</span></p>
+        <p class="max-temp"><span>MAX</span>${data.main.temp_max}<span>º</span></p>
+    </div>`;
+}
+
+async function loaderComponent() {
+  const loadScreen = document.querySelector(".loading-screen");
+
+  loadScreen.classList.toggle("active");
+}
